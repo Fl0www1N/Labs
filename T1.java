5a7329087c3048485dc3bb9a1ef97db7b0afae2b
Fl0www1N <166929662+Fl0www1N@users.noreply.github.com>
2026-01-14 21:50:07 +0300

    Update T1.java

--- T1.java
+++ T1.java
@@ -1,24 +1,25 @@
 import java.io.PrintStream;
 import java.util.Scanner;
 
-public class Task1 {
+public class T1 {
     public static Scanner in = new Scanner(System.in);
     public static PrintStream out = System.out;
     static String dv(int a){
         if (a == 0)
-            return "";
-        return dv(a/2) + ""  + a%2;
+            return " ";
+        return dv(a/2) + " "  + a % 2;
     }
+
     public static void main(String[] args) {
-        int q = in.nextInt();
-        for (int i = 1; i<=q; i++){
+        int n = in.nextInt();
+        for (int i = 1; i <= n; i++){
             String w = dv(i);
-            int e = 0;
+            int q = 0;
             for (int j = 0; j < w.length(); j++){
                 if (w.charAt(j) == '1')
-                    e++;
+                    q++;
             }
-            out.print(e + ", ");
+            out.print(q + " ");
         }
     }
 }
